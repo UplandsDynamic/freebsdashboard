18da0233c0165f1221c3c39c7dd843e418dd2e62
Dan Bright <productions@zaziork.com>
2016-06-21 20:43:30 +0100

    Update mirrored from development repository

--- update_github_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## updates files to mirror the main dev git repo
-chmod 000 .git;
-cp -pR /home/dan/Data/zwsDevelopment/PYTHON/PyCharm/PycharmProjects/django/freebsdashboard/* .;
-chmod 700 .git;
-git add .;
-git commit -am 'Update mirrored from development repository'
-git push origin master;
-echo "Update pushed!"
